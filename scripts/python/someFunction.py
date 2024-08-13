1b7b3393e36e066b1132ea6afad0b17e94a71a71
Mahya Mirbagheri <mirbagherim@PRISM-P358-1.PRISMLAB>
2024-08-13 13:22:52 -0400

    Initial commit - cleaned code and added documentation

--- scripts/python/someFunction.py
+++ scripts/python/someFunction.py
@@ -1,535 +1,127 @@
+import random
+import nltk
+import json
 
+# Set the NLTK data path
+nltk.data.path.append('.')
 
-
-import sys
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3\\python38.zip')
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3\\DLLs')
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3\\lib')
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3')
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3\\lib\\site-packages')
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3\\lib\\site-packages\\locket-0.2.1-py3.8.egg')
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3\\lib\\site-packages\\win32')
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3\\lib\\site-packages\\win32\\lib')
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3\\lib\\site-packages\\Pythonwin')
-sys.path.append('C:\\Users\\mirbagherim\\anaconda3\\lib\\site-packages\\IPython\\extensions')
-sys.path.append('C:\\Users\\mirbagherim\\.ipython')
-# In[11]:
-
-
-# UNQ_C1 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-### GRADED_FUNCTION: split_to_sentences ###
 def split_to_sentences(data):
     sentences = data.split('\n')
     sentences = [s.strip() for s in sentences]
     sentences = [s for s in sentences if len(s) > 0]
-
     return sentences
 
-
-
-
-# In[14]:
-
-
-# UNQ_C2 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-### GRADED_FUNCTION: tokenize_sentences ###
 def tokenize_sentences(sentences):
-
-    # Initialize the list of lists of tokenized sentences
     tokenized_sentences = []
-
-    # Go through each sentence
     for sentence in sentences:
-
-        # Convert to lowercase letters
         sentence = sentence.lower()
-
-        # Convert into a list of words
         tokenized = nltk.word_tokenize(sentence)
-
-        # append the list of words to the list of lists
         tokenized_sentences.append(tokenized)
-
-    ### END CODE HERE ###
-
     return tokenized_sentences
-
-
-# In[16]:
 
 def get_tokenized_data(data):
-    """
-    Make a list of tokenized sentences
-
-    Args:
-        data: String
-
-    Returns:
-        List of lists of tokens
-    """
-    ### START CODE HERE (Replace instances of 'None' with your code) ###
-
-    # Get the sentences by splitting up the data
     sentences = split_to_sentences(data)
-
-    # Get the list of lists of tokens by tokenizing the sentences
     tokenized_sentences = tokenize_sentences(sentences)
-
-    ### END CODE HERE ###
-
     return tokenized_sentences
 
-
-# UNQ_C4 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-### GRADED_FUNCTION: count_words ###
 def count_words(tokenized_sentences):
-    """
-    Count the number of word appearence in the tokenized sentences
-
-    Args:
-        tokenized_sentences: List of lists of strings
-
-    Returns:
-        dict that maps word (str) to the frequency (int)
-    """
-
     word_counts = {}
-    ### START CODE HERE (Replace instances of 'None' with your code) ###
-
-    # Loop through each sentence
-    for sentence in tokenized_sentences: # complete this line
-
-        # Go through each token in the sentence
-        for token in sentence: # complete this line
-
-            # If the token is not in the dictionary yet, set the count to 1
-            if token not in word_counts.keys(): # complete this line
-                word_counts[token] = 1
-
-            # If the token is already in the dictionary, increment the count by 1
-            else:
-                word_counts[token] += 1
-
-    ### END CODE HERE ###
-
+    for sentence in tokenized_sentences:
+        for token in sentence:
+            word_counts[token] = word_counts.get(token, 0) + 1
     return word_counts
 
-
-# In[22]:
-
-
-# UNQ_C5 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-### GRADED_FUNCTION: get_words_with_nplus_frequency ###
 def get_words_with_nplus_frequency(tokenized_sentences, count_threshold):
-    """
-    Find the words that appear N times or more
-
-    Args:
-        tokenized_sentences: List of lists of sentences
-        count_threshold: minimum number of occurrences for a word to be in the closed vocabulary.
-
-    Returns:
-        List of words that appear N times or more
-    """
-    # Initialize an empty list to contain the words that
-    # appear at least 'minimum_freq' times.
     closed_vocab = []
-
-    # Get the word couts of the tokenized sentences
-    # Use the function that you defined earlier to count the words
     word_counts = count_words(tokenized_sentences)
-
-    ### START CODE HERE (Replace instances of 'None' with your code) ###
-
-    # for each word and its count
-    for word, cnt in word_counts.items(): # complete this line
-
-        # check that the word's count
-        # is at least as great as the minimum count
+    for word, cnt in word_counts.items():
         if cnt >= count_threshold:
-
-            # append the word to the list
             closed_vocab.append(word)
-    ### END CODE HERE ###
-
     return closed_vocab
 
-
-# In[24]:
-
-
-# UNQ_C6 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-### GRADED_FUNCTION: replace_oov_words_by_unk ###
 def replace_oov_words_by_unk(tokenized_sentences, vocabulary, unknown_token="<unk>"):
-    """
-    Replace words not in the given vocabulary with '<unk>' token.
-
-    Args:
-        tokenized_sentences: List of lists of strings
-        vocabulary: List of strings that we will use
-        unknown_token: A string representing unknown (out-of-vocabulary) words
-
-    Returns:
-        List of lists of strings, with words not in the vocabulary replaced
-    """
-
-    # Place vocabulary into a set for faster search
     vocabulary = set(vocabulary)
-
-    # Initialize a list that will hold the sentences
-    # after less frequent words are replaced by the unknown token
     replaced_tokenized_sentences = []
-
-    # Go through each sentence
     for sentence in tokenized_sentences:
-
-        # Initialize the list that will contain
-        # a single sentence with "unknown_token" replacements
-        replaced_sentence = []
-        ### START CODE HERE (Replace instances of 'None' with your code) ###
-
-        # for each token in the sentence
-        for token in sentence: # complete this line
-
-            # Check if the token is in the closed vocabulary
-            if token in vocabulary: # complete this line
-                # If so, append the word to the replaced_sentence
-                replaced_sentence.append(token)
-            else:
-                # otherwise, append the unknown token instead
-                replaced_sentence.append(unknown_token)
-        ### END CODE HERE ###
-
-        # Append the list of tokens to the list of lists
+        replaced_sentence = [token if token in vocabulary else unknown_token for token in sentence]
         replaced_tokenized_sentences.append(replaced_sentence)
     return replaced_tokenized_sentences
 
-
-
-
-# In[26]:
-
-
-# UNQ_C7 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-### GRADED_FUNCTION: preprocess_data ###
 def preprocess_data(train_data, test_data, count_threshold):
-
-
-    # Get the closed vocabulary using the train data
-    vocabulary = get_words_with_nplus_frequency(train_data,count_threshold)
-
-    # For the train data, replace less common words with "<unk>"
-    train_data_replaced = replace_oov_words_by_unk(train_data,vocabulary)
-
-    # For the test data, replace less common words with "<unk>"
-    test_data_replaced = replace_oov_words_by_unk(test_data,vocabulary)
-
-    ### END CODE HERE ###
+    vocabulary = get_words_with_nplus_frequency(train_data, count_threshold)
+    train_data_replaced = replace_oov_words_by_unk(train_data, vocabulary)
+    test_data_replaced = replace_oov_words_by_unk(test_data, vocabulary)
     return train_data_replaced, test_data_replaced, vocabulary
 
-
-# In[28]:
-
-
-
-# UNQ_C8 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-### GRADED FUNCTION: count_n_grams ###
-def count_n_grams(data, n, start_token='<s>', end_token = '<e>'):
-    """
-    Count all n-grams in the data
-
-    Args:
-        data: List of lists of words
-        n: number of words in a sequence
-
-    Returns:
-        A dictionary that maps a tuple of n-words to its frequency
-    """
-
-    # Initialize dictionary of n-grams and their counts
+def count_n_grams(data, n, start_token='<s>', end_token='<e>'):
     n_grams = {}
-
-    ### START CODE HERE (Replace instances of 'None' with your code) ###
-
-    # Go through each sentence in the data
-    for sentence in data: # complete this line
-
-        # prepend start token n times, and  append <e> one time
-        sentence = [start_token] * n+ sentence + [end_token]
-
-        # convert list to tuple
-        # So that the sequence of words can be used as
-        # a key in the dictionary
+    for sentence in data:
+        sentence = [start_token] * n + sentence + [end_token]
         sentence = tuple(sentence)
-
-        # Use 'i' to indicate the start of the n-gram
-        # from index 0
-        # to the last index where the end of the n-gram
-        # is within the sentence.
-        m = len(sentence) if n==1 else len(sentence)-1
-        for i in range(m): # complete this line
-
-            # Get the n-gram from i to i+n
-            n_gram = sentence[i:i+n]
-
-            # check if the n-gram is in the dictionary
-            if n_gram in n_grams.keys(): # complete this line
-
-                # Increment the count for this n-gram
-                n_grams[n_gram] += 1
-            else:
-                # Initialize this n-gram count to 1
-                n_grams[n_gram] = 1
-
-            ### END CODE HERE ###
+        m = len(sentence) if n == 1 else len(sentence) - 1
+        for i in range(m):
+            n_gram = sentence[i:i + n]
+            n_grams[n_gram] = n_grams.get(n_gram, 0) + 1
     return n_grams
 
-
-
-
-# In[32]:
-
-
-# UNQ_C9 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-### GRADED FUNCTION: estimate_probabilityy ###
-def estimate_probability(word, previous_n_gram,
-                         n_gram_counts, n_plus1_gram_counts, vocabulary_size, k=1.0):
-
-    # convert list to tuple to use it as a dictionary key
+def estimate_probability(word, previous_n_gram, n_gram_counts, n_plus1_gram_counts, vocabulary_size, k=1.0):
     previous_n_gram = tuple(previous_n_gram)
-
-    ### START CODE HERE (Replace instances of 'None' with your code) ###
-
-    # Set the denominator
-    # If the previous n-gram exists in the dictionary of n-gram counts,
-    # Get its count.  Otherwise set the count to zero
-    # Use the dictionary that has counts for n-grams
-    previous_n_gram_count = n_gram_counts[previous_n_gram] if previous_n_gram in n_gram_counts  else 0
-
-    # Calculate the denominator using the count of the previous n gram
-    # and apply k-smoothing
+    previous_n_gram_count = n_gram_counts.get(previous_n_gram, 0)
     denominator = previous_n_gram_count + k * vocabulary_size
-
-    # Define n plus 1 gram as the previous n-gram plus the current word as a tuple
     n_plus1_gram = previous_n_gram + (word,)
-
-    # Set the count to the count in the dictionary,
-    # otherwise 0 if not in the dictionary
-    # use the dictionary that has counts for the n-gram plus current word
-    n_plus1_gram_count = n_plus1_gram_counts[n_plus1_gram] if n_plus1_gram in n_plus1_gram_counts  else 0
-
-    # Define the numerator use the count of the n-gram plus current word,
-    # and apply smoothing
+    n_plus1_gram_count = n_plus1_gram_counts.get(n_plus1_gram, 0)
     numerator = n_plus1_gram_count + k
-
-    # Calculate the probability as the numerator divided by denominator
     probability = numerator / denominator
-
-    ### END CODE HERE ###
-
     return probability
 
-
-# In[33]:
-
-
-
-# In[34]:
-
-
 def estimate_probabilities(previous_n_gram, n_gram_counts, n_plus1_gram_counts, vocabulary, k=1.0):
-    """
-    Estimate the probabilities of next words using the n-gram counts with k-smoothing
-
-    Args:
-        previous_n_gram: A sequence of words of length n
-        n_gram_counts: Dictionary of counts of (n+1)-grams
-        n_plus1_gram_counts: Dictionary of counts of (n+1)-grams
-        vocabulary: List of words
-        k: positive constant, smoothing parameter
-
-    Returns:
-        A dictionary mapping from next words to the probability.
-    """
-
-    # convert list to tuple to use it as a dictionary key
     previous_n_gram = tuple(previous_n_gram)
-
-    # add <e> <unk> to the vocabulary
-    # <s> is not needed since it should not appear as the next word
     vocabulary = vocabulary + ["<e>", "<unk>"]
     vocabulary_size = len(vocabulary)
-
-    probabilities = {}
-    for word in vocabulary:
-        probability = estimate_probability(word, previous_n_gram,
-                                           n_gram_counts, n_plus1_gram_counts,
-                                           vocabulary_size, k=k)
-        probabilities[word] = probability
-
+    probabilities = {word: estimate_probability(word, previous_n_gram, n_gram_counts, n_plus1_gram_counts, vocabulary_size, k=k) for word in vocabulary}
     return probabilities
 
-
-
-
-
-# In[45]:
-
-
-# UNQ_C11 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-# GRADED FUNCTION: suggest_a_word
 def suggest_a_word(previous_tokens, n_gram_counts, n_plus1_gram_counts, vocabulary, k=1.0, start_with=None):
-    """
-    Get suggestion for the next word
-
-    Args:
-        previous_tokens: The sentence you input where each token is a word. Must have length > n
-        n_gram_counts: Dictionary of counts of (n+1)-grams
-        n_plus1_gram_counts: Dictionary of counts of (n+1)-grams
-        vocabulary: List of words
-        k: positive constant, smoothing parameter
-        start_with: If not None, specifies the first few letters of the next word
-
-    Returns:
-        A tuple of
-          - string of the most likely next word
-          - corresponding probability
-    """
-
-    # length of previous words
     n = len(list(n_gram_counts.keys())[0])
-
-    # From the words that the user already typed
-    # get the most recent 'n' words as the previous n-gram
     previous_n_gram = previous_tokens[-n:]
-
-    # Estimate the probabilities that each word in the vocabulary
-    # is the next word,
-    # given the previous n-gram, the dictionary of n-gram counts,
-    # the dictionary of n plus 1 gram counts, and the smoothing constant
-    probabilities = estimate_probabilities(previous_n_gram,
-                                           n_gram_counts, n_plus1_gram_counts,
-                                           vocabulary, k=k)
-
-    # Initialize suggested word to None
-    # This will be set to the word with highest probability
+    probabilities = estimate_probabilities(previous_n_gram, n_gram_counts, n_plus1_gram_counts, vocabulary, k=k)
     suggestion = None
-
-    # Initialize the highest word probability to 0
-    # this will be set to the highest probability
-    # of all words to be suggested
     max_prob = 0
-
-    ### START CODE HERE (Replace instances of 'None' with your code) ###
-
-    # For each word and its probability in the probabilities dictionary:
-    for word, prob in probabilities.items(): # complete this line
-
-        # If the optional start_with string is set
-        if start_with != None: # complete this line
-
-            # Check if the beginning of word does not match with the letters in 'start_with'
-            if not word.startswith(start_with): # complete this line
-
-                # if they don't match, skip this word (move onto the next word)
-                continue  # complete this line
-
-        # Check if this word's probability
-        # is greater than the current maximum probability
-        if prob > max_prob: # complete this line
-
-            # If so, save this word as the best suggestion (so far)
+    for word, prob in probabilities.items():
+        if start_with and not word.startswith(start_with):
+            continue
+        if prob > max_prob:
             suggestion = word
-
-            # Save the new maximum probability
             max_prob = prob
-
-    ### END CODE HERE
-
     return suggestion, max_prob
 
-
-# In[47]:
-
-
 def get_suggestions(previous_tokens, n_gram_counts_list, vocabulary, k=1.0, start_with=None):
-    model_counts = len(n_gram_counts_list)
     suggestions = []
-    for i in range(model_counts-1):
+    for i in range(len(n_gram_counts_list) - 1):
         n_gram_counts = n_gram_counts_list[i]
-        n_plus1_gram_counts = n_gram_counts_list[i+1]
-
-        suggestion = suggest_a_word(previous_tokens, n_gram_counts,
-                                    n_plus1_gram_counts, vocabulary,
-                                    k=k, start_with=start_with)
+        n_plus1_gram_counts = n_gram_counts_list[i + 1]
+        suggestion = suggest_a_word(previous_tokens, n_gram_counts, n_plus1_gram_counts, vocabulary, k=k, start_with=start_with)
         suggestions.append(suggestion)
     return suggestions
 
-# In[49]:
+# Example usage
+if __name__ == "__main__":
+    with open('[data path]', "r", encoding="utf8") as f:
+        data = f.read()
 
+    tokenized_data = get_tokenized_data(data)
+    random.seed(87)
+    random.shuffle(tokenized_data)
 
-import random
+    train_size = int(len(tokenized_data) * 0.8)
+    train_data = tokenized_data[:train_size]
+    test_data = tokenized_data[train_size:]
+    minimum_freq = 2
+    train_data_processed, test_data_processed, vocabulary = preprocess_data(train_data, test_data, minimum_freq)
 
-import nltk
+    n_gram_counts_list = [count_n_grams(train_data_processed, n) for n in range(1, 6)]
 
-nltk.data.path.append('.')
-
-import json
-json_object = json.dumps("g")
-with open('C:\\Users\\mirbagherim\\Downloads\\data\\tmp.json', "w+") as outfile:
-    outfile.write(json_object)
-with open('C:\\Users\\mirbagherim\\Downloads\\data\\crps\\TwitterConvCorpus.txt', "r", encoding="utf8") as f:
-    data = f.read()
-
-
-
-tokenized_data = get_tokenized_data(data)
-random.seed(87)
-random.shuffle(tokenized_data)
-
-train_size = int(len(tokenized_data) * 0.8)
-train_data = tokenized_data[0:train_size]
-test_data = tokenized_data[train_size:]
-minimum_freq = 2
-train_data_processed, test_data_processed, vocabulary = preprocess_data(train_data,
-                                                                            test_data,
-                                                                            minimum_freq)
-
-n_gram_counts_list = []
-for n in range(1, 6):
-    #print("Computing n-gram counts with n =", n, "...")
-    n_model_counts = count_n_grams(train_data_processed, n)
-    n_gram_counts_list.append(n_model_counts)
-
-
-
-
-
-# In[12]:
-
-
-while (1):
-    with open('C:\\Users\\mirbagherim\\Downloads\\data\\tmp.json',"r", encoding="utf8") as load_obj:
-         previous_tokens= json.load(load_obj)
-    if (previous_tokens):
-        lst_wrd = previous_tokens[-1]
-
-        tmp_suggest8 = get_suggestions(previous_tokens[0:-1], n_gram_counts_list, vocabulary, k=1.0,start_with=lst_wrd)
-        str = [tmp_suggest8[0][0],tmp_suggest8[1][0],tmp_suggest8[2][0]];
-        #str = ["do", "doing","does"];
-        str = set(str)
-        """
-        if tmp_suggest8[0][0] == lst_wrd:
-            lst_wrd = ""
-            tmp_suggest8 = get_suggestions(previous_tokens, n_gram_counts_list, vocabulary, k=1.0, start_with=lst_wrd)
-        """
-        x = {
-          "last_word": lst_wrd,
-          "Predicted_num": len(str),
-          "Words": list(str)
-        }
-        json_object = json.dumps(x)
-
-        with open('C:\\Users\\mirbagherim\\Downloads\\data\\ans.json', "w") as outfile:
-            outfile.write(json_object)
-
+    # Example of how to get suggestions
+    previous_tokens = ["hello", "how", "are"]
+    suggestions = get_suggestions(previous_tokens, n_gram_counts_list, vocabulary, k=1.0, start_with="y")
+    print(suggestions)
